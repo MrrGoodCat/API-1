ccaa7c0ebf92bee9b6a316b960ecbc62078153ca
MrrGoodCat <mrrgoodcat@gmail.com>
2018-09-18 00:55:39 +0300

    Metric Collector (work started)

---
+++ SentinelAPICore/MetricsCollector.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AgentsCenter.Information;
+
+namespace SentinelAPICore
+{
+    public class MetricsCollector
+    {
+        List<AgentCurrentStatus> Agents = new List<AgentCurrentStatus>();
+
+    }
+}
